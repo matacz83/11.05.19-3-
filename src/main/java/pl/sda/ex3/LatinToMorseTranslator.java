d87dc8e76f471109d4a742105b2b557e02766256
matacz <matacz@gmail.com>
2019-05-11 15:57:09 +0200

    Initial commit

---
+++ src/main/java/pl/sda/ex3/LatinToMorseTranslator.java
@@ -0,0 +1,47 @@
+package pl.sda.ex3;
+
+import java.util.Map;
+
+class LatinToMorseTranslator implements Translator {
+
+    private static final String CHARACTER_SPACE = " ";
+    private static final String CHARACTER_SLASH = "/";
+    private Map<String, String> dictionary;
+
+    LatinToMorseTranslator(Map<String, String> dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    @Override
+    public String translate(String text) {
+        String[] words = text.split(CHARACTER_SPACE);
+
+        StringBuffer stringBuffer = new StringBuffer();
+        int counterOfProcessedWords = 1;
+        for (String word : words) {
+            String translatedWord = translateWord(word);
+            stringBuffer.append(translatedWord);
+
+            if (counterOfProcessedWords < words.length) {
+                stringBuffer.append(CHARACTER_SLASH);
+                stringBuffer.append(CHARACTER_SPACE);
+            }
+
+            counterOfProcessedWords++;
+        }
+
+        return stringBuffer.toString().trim();
+    }
+
+    private String translateWord(String word) {
+        StringBuffer stringBuffer = new StringBuffer();
+
+        for (Character character : word.toCharArray()) {
+            String translatedCharacter = (dictionary.get(character.toString().toUpperCase()));
+            stringBuffer.append(translatedCharacter);
+            stringBuffer.append(CHARACTER_SPACE);
+        }
+
+        return stringBuffer.toString();
+    }
+}
---
+++ src/main/java/pl/sda/ex3/Translator.java
@@ -0,0 +1,5 @@
+package pl.sda.ex3;
+
+public interface Translator {
+    String translate(String word);
+}
