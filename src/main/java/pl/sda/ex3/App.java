9e7de600e4619688eb904eaefdfdc06c574c97f5
matacz <matacz@gmail.com>
2019-05-12 10:13:41 +0200

    file reader

---
+++ src/main/java/pl/sda/ex3/App.java
@@ -0,0 +1,13 @@
+package pl.sda.ex3;
+
+public class App {
+    public static void main(String[] args){
+        FileLatinMorseReader fileLatinMorseReader = new FileLatinMorseReader();
+        LatinToMorseTranslator translator = new LatinToMorseTranslator(fileLatinMorseReader.getDictionary());
+
+        String textToTranslate = "SOS";
+
+        System.out.println(translator.translate(textToTranslate));
+
+    }
+}
